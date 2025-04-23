61cb6e3e4cbfa525cbd63d3fbba59278e2b8b0e0
Aliaksei <sladkiy.aleksey@gmail.com>
2025-04-23 20:43:20 +0200

    feat: copy folder

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { cp } from "node:fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+  const src = join(__dirname, "files");
+  const dest = join(__dirname, "files_copy");
+  const options = {
+    force: false,
+    recursive: true,
+    errorOnExist: true,
+  };
+
+  return cp(src, dest, options).catch(() => {
+    console.log("FS operation failed");
+  });
 };
 
 await copy();
