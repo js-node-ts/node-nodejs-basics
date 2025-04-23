d458ca9ce33bf698a60cabb66c119caeffa86124
Aliaksei <sladkiy.aleksey@gmail.com>
2025-04-23 20:18:42 +0200

    feat: create new file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { writeFile } from "node:fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const text = "I am fresh and young";
+  const path = join(__dirname, "files", "fresh.txt");
+  const options = {
+    flag: "wx",
+  };
+
+  return writeFile(path, text, options).catch(() => {
+    console.log("FS operation failed");
+  });
 };
 
 await create();
