e4dbc346d5c5ae9a7076003623599456a06abf89
Aliaksei <sladkiy.aleksey@gmail.com>
2025-04-25 23:27:27 +0200

    feat: print array of all filenames

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { readdir } from "node:fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+  const path = join(__dirname, "files");
+
+  return readdir(path)
+    .then((list) => console.log(list))
+    .catch(() => {
+      throw new Error("FS operation failed");
+    });
 };
 
 await list();
