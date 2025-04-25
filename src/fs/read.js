4494649933b17c1dfffba61b595651fe348483f9
Aliaksei <sladkiy.aleksey@gmail.com>
2025-04-26 00:06:58 +0200

    feat:  print content

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { readFile } from "node:fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const path = join(__dirname, "files", "fileToRead.txt");
+  const options = {
+    encoding: "utf8",
+  };
+
+  return readFile(path, options)
+    .then((data) => console.log(data))
+    .catch(() => {
+      throw new Error("FS operation failed");
+    });
 };
 
 await read();
