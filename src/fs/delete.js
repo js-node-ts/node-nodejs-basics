9574ea102eadeca122da0a63e152ba75a85085ca
Aliaksei <sladkiy.aleksey@gmail.com>
2025-04-25 22:46:48 +0200

    feat: delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { rm } from "node:fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  const path = join(__dirname, "files", "fileToRemove.txt");
+
+  return rm(path).catch(() => {
+    throw new Error("FS operation failed");
+  });
 };
 
 await remove();
