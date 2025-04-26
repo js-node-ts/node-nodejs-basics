7a17c3cfa224aed8fb7c5f414204f0c99637e42e
Aliaksei <sladkiy.aleksey@gmail.com>
2025-04-27 00:00:16 +0200

    feat: parse environment variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+  const variables = Object.entries(process.env)
+    .reduce((acc, [key, value]) => {
+      if (key.startsWith("RSS_")) {
+        const item = `${key}=${value}`;
+        acc.push(item);
+      }
+      return acc;
+    }, [])
+    .join("; ");
+
+  console.log(variables);
 };
 
 parseEnv();
