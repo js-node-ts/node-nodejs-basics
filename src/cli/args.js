68913aedfb95e9f9a7ffe4b5f9fc9f7308ca1f09
Aliaksei <sladkiy.aleksey@gmail.com>
2025-04-27 00:18:00 +0200

    feat:  parse command line arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv
+    .slice(2)
+    .reduce((acc, item, index, array) => {
+      if (item.startsWith("--")) {
+        const value = `${item.slice(2)} is ${array[index + 1]}`;
+        acc.push(value);
+      }
+      return acc;
+    }, [])
+    .join(", ");
+
+  console.log(args);
 };
 
 parseArgs();
