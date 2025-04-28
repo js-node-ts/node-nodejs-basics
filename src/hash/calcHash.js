71aa4e63abadebb8ad0c7aca146fa6165c31b203
Aliaksei <sladkiy.aleksey@gmail.com>
2025-04-28 21:40:55 +0200

    feat: calculates SHA256 hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
+import { Transform } from "stream";
+import { pipeline } from "stream/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const path = join(__dirname, "files", "fileToCalculateHashFor.txt");
+  const readStream = createReadStream(path);
+
+  const transformStream = new Transform({
+    transform(chunk, _, callback) {
+      const hash = createHash("sha256").update(chunk).digest("hex");
+
+      callback(null, hash);
+    },
+  });
+
+  pipeline(readStream, transformStream, process.stdout);
 };
 
 await calculateHash();
