f8db7ff5dd75f2b94bc39a85a64812126cdacec4
Aliaksei <sladkiy.aleksey@gmail.com>
2025-04-28 23:22:17 +0200

    feat: implement child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { spawn } from "child_process";
+import { pipeline } from "stream/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const path = join(__dirname, "files", "script.js");
+  const childProcess = spawn("node", [path, ...(args ?? [])]);
+
+  await Promise.all([
+    pipeline(process.stdin, childProcess.stdin),
+    pipeline(childProcess.stdout, process.stdout),
+  ]);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
